a2cf3d18201b110c478d78f94bfc4a68168c7adb
thao <roseskindergarten@gmail.com>
2015-12-27 02:31:32 -0500

    Update s3.rb

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -5,8 +5,8 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => Rails.application.secrets.amazon_access_key_id,
       :aws_secret_access_key  => Rails.application.secrets.amazon_secret_access_key,
 =end
-      :aws_access_key_id      => 'AKIAILHZPGIOTG2R7KKA',
-      :aws_secret_access_key  => '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',
+      :aws_access_key_id      => '',
+      :aws_secret_access_key  => '',
       :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = "user-upload786"
